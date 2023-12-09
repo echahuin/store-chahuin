acdffb9224580767a8d736c59b77c7e984fe48a0
Edgar <edgachahuinauccafuro@frba.utn.edu.ar>
2023-12-09 05:10:47 -0300

    d

--- app/utils/getDataBanner.js
+++ app/utils/getDataBanner.js
@@ -1,8 +1,8 @@
 const getDataBanner = async (typeBanner) => {
 
   
-  const url = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
-    const response = await fetch(`${url}/api/typeBanners/${typeBanner}`);
+  // const url = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/typeBanners/${typeBanner}`);
     if (!response.ok) {
       console.error(`Failed to fetch data: ${response.status}`);
       return [];
