7d81deeaaa0ee20fba263f78c158c0c63e1420ca
Edgar <edgachahuinauccafuro@frba.utn.edu.ar>
2023-12-10 03:17:39 -0300

    a

--- app/utils/getProductCategoryId.js
+++ app/utils/getProductCategoryId.js
@@ -3,7 +3,7 @@ const getProductCategory = async (params) => {
 
     const {category, id} = params
 
-    const response = await fetch(`http://localhost:3000/api/products/${category}/description/${id}`);
+    const response = await fetch(`http://${process.env.VERCEL_URL}/api/products/${category}/description/${id}`);
     if (!response.ok) {
         console.error(`Failed to fetch data: ${response.status}`);
         return [];
