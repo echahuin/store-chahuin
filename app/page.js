8a96f80052d5d868034590e4d1b624662aebda00
Edgar <edgachahuinauccafuro@frba.utn.edu.ar>
2023-12-28 12:13:13 -0300

    update fetch

--- app/page.js
+++ app/page.js
@@ -1,73 +1,13 @@
-// import Banner from './components/Banner'
-// import PrincipalBanner from './components/PrincipalBanner'
-// import Footer from './components/UI/Footer'
-// import SectionBanner from './components/SectionBanner'
-// import getDataBanner from './utils/getDataBanner'
-
-
-// const getServerSideProps = async () => {
-//   const data = await getDataBanner('big-1');
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// };
-
-// const Home = async({data}) => {
-
-//   return (
-//       <div style={{ background: '#F1F1F1' }}>
-//         <PrincipalBanner />  
-//           <SectionBanner products={data} />
-//           {/* <Banner products={bigBannerData} />
-//           <SectionBanner products={smallsBannerData2} />
-//           <Banner products={bigBannerData2} /> */}
-//         <Footer /> 
-//       </div>
-//   );
-// };
-
-// export  default Home
-
-
 import Banner from './components/Banner';
 import PrincipalBanner from './components/PrincipalBanner';
 import Footer from './components/UI/Footer';
-import SectionBanner from './components/SectionBanner';
+// import SectionBanner from './components/SectionBanner';
 import { Suspense } from 'react';
-// import getDataBanner from './utils/getDataBanner';
 
-// export async function getData() {
-//   const [data, error] = await Promise.all([
-//     // getDataBanner('big-1'),
-//     getDataBanner('small-1'),
-//     // getDataBanner('small-2'),
-//     // getDataBanner('big-2'),
-//   ]);
-  
-//   console.log(data)
-//   console.log(error)
-
-//   if (error) {
-//     // Manejar el error de forma adecuada, por ejemplo, mostrando un mensaje de error
-//     return {
-//       props: {
-//         error: true,
-//       },
-//     };
-//   }
-
-//   return {
-//        data
-//       }
-// }
 const getDataBanner = async (typeBanner) => {
   try {
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/typeBanners/${typeBanner}`, {
-      cache: "no-store",
-    });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/typeBanners/${typeBanner}`);
 
     if (!response.ok) {
       throw new Error(`Failed to fetch data: ${response.status}`);
