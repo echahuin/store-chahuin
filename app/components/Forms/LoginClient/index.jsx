ee56f9ff0e8c13c510457fd92fdd6593aebbae36
Edgar <edgachahuinauccafuro@frba.utn.edu.ar>
2024-01-06 03:31:39 -0300

    update style form

--- app/components/Forms/LoginClient/index.jsx
+++ app/components/Forms/LoginClient/index.jsx
@@ -31,61 +31,29 @@ const LoginClient = () => {
 
   return (
     <div className={styles.contTypeLogin}>
-    <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div>
-          <div className="border-b border-gray-900/10 pb-8 pt-11">
-          <h2 className="text-center font-semibold leading-7 text-gray-900 pb-9">Ingresá</h2>
-          <div className="mt-19 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            {/* <div className="sm:col-span-3">
-              <label for="displayName" className="block text-sm font-medium leading-6 text-gray-900">Nombre</label>
-              <div className="mt-2">
-                <input onChange={handleChange} type="text" name="displayName" id="displayName" autocomplete="given-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          <div className="border-b border-gray-900/10 p-5 pb-8 pt-11">
+            <h2 className="text-center font-semibold leading-7 text-gray-900 pb-9">Ingresá</h2>
+            <div className="mt-19 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
+                <div className="mt-2">
+                  <input onChange={handleChange} type="text" name="email" id="email" autocomplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+  
+              <div className="sm:col-span-3">
+                <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
+                <div className="mt-2">
+                  <input onChange={handleChange} type="password" name="password" id="password" autocomplete="phone-number" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
               </div>
             </div>
-
-            <div className="sm:col-span-3">
-              <label for="phoneNumber" className="block text-sm font-medium leading-6 text-gray-900">Número de telefono</label>
-              <div className="mt-2">
-                <input onChange={handleChange} type="text" name="phoneNumber" id="phoneNumber" autocomplete="phone-number" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div> */}
-
-            <div className="sm:col-span-3">
-              <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
-              <div className="mt-2">
-                <input onChange={handleChange} type="text" name="email" id="email" autocomplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-
-            <div className="sm:col-span-3">
-              <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
-              <div className="mt-2">
-                <input onChange={handleChange} type="password" name="password" id="password" autocomplete="phone-number" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-
-            {/* <div className="sm:col-span-4">
-              <label for="file" className="block text-sm font-medium leading-6 text-gray-900">Foto de perfil</label>
-              <div className="mt-2">
-                <input 
-                  type="file" 
-                  accept="image/*" 
-                  allowMultiple={false}
-                  required 
-                  className='block w-full border p-2 my-2' 
-                  onChange={(e) => setFile(e.target.files[0])}
-                />               
-              </div>
-            </div> */}
           </div>
         </div>
-      </div>
-      <ButtonSmall type="submit" text="Ingresar" />
-    </form>
-  
-    <div>
-      
-    </div>
+        <ButtonSmall type="submit" text="Ingresar" />
+      </form>
     </div>
   );
 }
--- app/components/Forms/RegisterClient/index.jsx
+++ app/components/Forms/RegisterClient/index.jsx
@@ -35,14 +35,38 @@ const RegisterClient = () => {
         console.log('error')
       }
     };
+    
+  //   <form onSubmit={handleSubmit}>
+  //   <div>
+  //     <div className="border-b border-gray-900/10 p-5 pb-8 pt-11">
+  //       <h2 className="text-center font-semibold leading-7 text-gray-900 pb-9">Ingresá</h2>
+  //       <div className="mt-19 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+  //         <div className="sm:col-span-3">
+  //           <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
+  //           <div className="mt-2">
+  //             <input onChange={handleChange} type="text" name="email" id="email" autocomplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+  //           </div>
+  //         </div>
+
+  //         <div className="sm:col-span-3">
+  //           <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
+  //           <div className="mt-2">
+  //             <input onChange={handleChange} type="password" name="password" id="password" autocomplete="phone-number" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  //   <ButtonSmall type="submit" text="Ingresar" />
+  // </form>
   
     return (
       <div className={styles.contTypeRegister}>
       <form onSubmit={handleSubmit}>
           <div>
-            <div className="border-b border-gray-900/10 pb-8 pt-11">
+            <div className="border-b border-gray-900/10 p-5 pb-8 pt-11">
             <h2 className="text-center font-semibold leading-7 text-gray-900 pb-9">Necesitas Registrate</h2>
-            <div className="mt-19 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="mt-19 grid grid-cols-1 gap-x-2 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-3">
                 <label for="displayName" className="block text-sm font-medium leading-6 text-gray-900">Nombre</label>
                 <div className="mt-2">
@@ -71,15 +95,16 @@ const RegisterClient = () => {
                 </div>
               </div>
   
-              <div className="sm:col-span-4">
+              <div className="sm:col-span-3">
                 <label for="file" className="block text-sm font-medium leading-6 text-gray-900">Foto de perfil</label>
                 <div className="mt-2">
                   <input 
                     type="file" 
                     accept="image/*" 
                     allowMultiple={false}
-                    required 
-                    className='block w-full border p-2 my-2' 
+                    required
+                    className={styles.fotoPerfilInput}
+                    // className="block w-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
                     onChange={(e) => setFile(e.target.files[0])}
                   />               
                 </div>
@@ -89,10 +114,6 @@ const RegisterClient = () => {
         </div>
         <ButtonSmall type="submit" text="Registrarme" />
       </form>
-    
-      <div>
-        
-      </div>
       </div>
     );
 }
